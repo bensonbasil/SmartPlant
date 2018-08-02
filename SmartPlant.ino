95fce460fc0c7cee9d96fda241fa051523b6573e
Benson Basil <basilb@deakin.edu.au>
2018-08-02 11:54:46 +1000

    Add files via upload

---
+++ SmartPlant.ino
@@ -0,0 +1,50 @@
+  /*
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
+  # the sensor value description
+  # 0  ~300     dry soil (SOS)
+  # 300~700     humid soil (Off)
+  # 700~950     in water (SOS) 
+  */
+void setup() {
+  Serial.begin(1200); // open serial port, set the baud rate as 9600 bps
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  int value;
+  value = analogRead(0); //connect sensor to Analog 0
+  Serial.println(value); //print the value to serial port 
+  if (value < 300)
+  {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(3000); 
+  }
+  else (value > 700); 
+  {
+     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(3000); 
+
+  }
+  
